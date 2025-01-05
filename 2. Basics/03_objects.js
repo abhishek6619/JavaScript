9b9afb580f670af34b3af71d1ff086723855954c
Abhishek Kumar Singh <abhis42124@gmail.com>
2025-01-05 11:05:45 +0530

    Add Code

--- 2. Basics/03_objects.js
+++ 2. Basics/03_objects.js
@@ -2,3 +2,35 @@
 // Object.create
 
 // object literals
+
+const mySym = Symbol("Key1");
+
+const JsUser = {
+  name: "Abhishek",
+  "full name": "Abhishek Singh",
+  [mySym]: "mykey1",
+  age: 18,
+  location: "Dhanbad",
+  email: "abhis42124@gmail.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"],
+};
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "abhishek@chatgpt.com";
+// Object.freeze(JsUser);
+JsUser.email = "abhishek@microsoft.com";
+// console.log(JsUser);
+
+JsUser.greeting = function () {
+  console.log("Hello Js User");
+};
+JsUser.greetingTwo = function () {
+  console.log(`Hello Js User, ${this.name}`);
+};
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
