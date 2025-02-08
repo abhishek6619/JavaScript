87c8fd1f24fd34b9a1cce1ed1c6f6d148f445b3a
Abhishek Kumar Singh <abhis42124@gmail.com>
2025-02-08 22:44:49 +0530

    Update the Code

---
+++ 3. basics/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+
+//   function two() {
+//     const website = "youtube";
+//     console.log(username);
+//   }
+//   console.log(website);
+
+//   two();
+// }
+// one();
+
+if (true) {
+  const username = "Abhishek";
+  if (username === "Abhishek") {
+    const website = " youtube";
